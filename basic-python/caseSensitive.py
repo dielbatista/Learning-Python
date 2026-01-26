a4fcf74f658fe6d06c106ba83beada81ced19d29
dielbatista <dielxlem229@gmail.com>
2026-01-26 16:54:24 -0300

    learning the basics

---
+++ basic-python/caseSensitive.py
@@ -0,0 +1,18 @@
+#Python Language is a case sensitive programming language.
+
+#example Banana is different to banana
+
+Banana = "Banana"
+banana = "banana"
+
+if banana == Banana:
+    print("Banana equals to banana")
+else:
+    print("The following strings are different.")
+
+#Another Example
+
+age = 20
+Age = 31
+print(age)
+print(Age)
---
+++ basic-python/variables.py
@@ -0,0 +1,17 @@
+#declarete a variable
+numberinterger = 1000 #creates a interger variable
+numberFloat = 999.88 #creates a float variable
+name_eman = "Ceaser" #creates a string variable
+
+#print a variable
+print(numberinterger)
+print(numberFloat)
+print(name_eman)
+
+# del numberinterger #this line deletes a variable and we cant access to print.
+# print(numberinterger) #try to print the deleted variable generates and ERROR
+
+#Getting typles of variable
+print(type(numberinterger)) #type Interger
+print(type(numberFloat)) #type Float
+print(type(name_eman)) #type String
