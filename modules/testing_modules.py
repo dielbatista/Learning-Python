a0dad25bee2b5ef8e194b3ba1085743dc1d08466
dielbatista <dielxlem229@gmail.com>
2026-04-08 15:27:10 -0300

    modules_1

--- modules/testing_modules.py
+++ modules/testing_modules.py
@@ -21,3 +21,15 @@ in python, we can also import all names definitions from a module using the foll
 ##from math import * #This imports all names from the math module into the current namespace
 #print("The value of pi is:", pi) #Now we can use pi directly without the math. prefix
 
+
+
+
+
+"""The dir() built-in function
+in python, we can use the di() function to list all function names in a module
+for example, earlier we have defined the add() function in the modules example_modules.py, we can use the dir() function to list all names 
+in the example_modules."""
+
+print(dir(example_modules))
+#the follwoing result is: ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'add']
+print(example_modules.__name__)
