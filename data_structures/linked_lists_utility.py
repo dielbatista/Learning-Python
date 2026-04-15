539eddcddb290b92c422d6d46ab0955866ec0207
dielbatista <dielxlem229@gmail.com>
2026-04-15 14:51:40 -0300

    linked_lists_utility.py

---
+++ data_structures/linked_lists_utility.py
@@ -0,0 +1,7 @@
+"""Linked list utility
+lists are one of the most popular and efficient data structures, with implementation in
+every programming language like C, C++, Python, Java and C#
+apart from that, linked lists are a great way to learn how pointers work.
+by praticing how to manipulate linked lists, you can prepare yourself to learn more advanced data structures
+like graphs and trees."""
+
