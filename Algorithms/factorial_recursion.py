b019d18206b325303c5bf604059bdcfa98711c7e
dielbatista <dielxlem229@gmail.com>
2026-04-17 13:59:50 -0300

    factorial_recursion

--- Algorithms/factorial_recursion.py
+++ Algorithms/factorial_recursion.py
@@ -21,6 +21,23 @@ progressively approaches the base case."""
 #Define a Python factorial function
 
 def factorial(n):
-    return 1 if n <= 1 else n * factorial(n - 1)
+    print(f"factorial({n}) called with n = {n} ")
+    return_value = 1 if n <= 1 else n * factorial(n - 1)
+    print(f"returning {return_value} for factorial({n})")
+    return return_value
 
-print(factorial(4))
+print(factorial(10))
+
+"""notice how all the recursive calls stack up. the function gets called with n = 4, 3, 2 and 1
+in succession before any of the calls return. Finally, when n is 1, the problem can be solved without any 
+more recursion. then each of the stack-up recursive calls uniwnds back ou, returning 1, 2, 6 and finally 24 from the outermost call
+
+recursion ins't necessary here. You could implement factorial() iteratively using a for loop
+"""
+def facttorial(m):
+    return_value = 1
+    for i in range(2, m + 1):
+        return_value *= i
+    return return_value
+
+facttorial(4)
